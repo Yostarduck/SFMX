345ea8b00424a1c74a459946e84bf2b77c48f1fc
Accel <accel.mr@gmail.com>
2026-06-03 22:13:27 -0600

    Refactor FrameworkGame: Remove test component and add UUID and TypeTraits utilities

---
+++ FrameworkGame/include/utils/UUID.h
@@ -0,0 +1,72 @@
+
+#pragma once
+
+#include "core/platform/Prerequisites.h"
+
+#define STDUUID_FORCE_IMPLEMENTATION
+#define UUID_SYSTEM_GENERATOR
+#include "stdUUID/uuid.h"
+
+namespace sfmx {
+
+class SFMX_UTILITY_EXPORT UUID  {
+ public:
+  UUID() = default;
+
+  //explicit UUID(const String& str);
+
+  NODISCARD FORCEINLINE bool
+  isNull() const { return m_uuid.is_nil(); }
+
+  NODISCARD FORCEINLINE String
+  toString() const { return uuids::to_string(m_uuid); }
+
+  NODISCARD FORCEINLINE bool
+  operator==(const UUID& other) const { return m_uuid == other.m_uuid; }
+
+  NODISCARD FORCEINLINE bool
+  operator!=(const UUID& other) const { return m_uuid != other.m_uuid; }
+
+  NODISCARD FORCEINLINE bool
+  operator<(const UUID& other) const { return m_uuid < other.m_uuid; }
+
+  NODISCARD FORCEINLINE SIZE_T
+  getHash() const noexcept { return Hash<uuids::uuid>{}(m_uuid); }
+
+  NODISCARD static UUID
+  createRandom();
+
+  NODISCARD static UUID
+  createFromName(const String& name, const UUID& namespace_uuid = UUID::null());
+
+  NODISCARD FORCEINLINE static const UUID&
+  null() { static UUID nullUUID(uuids::uuid{}); return nullUUID; }
+
+ protected:
+
+  explicit UUID(const uuids::uuid& uuid)
+    : m_uuid(uuid) {}
+
+ private:
+  friend Hash<UUID>;
+  uuids::uuid m_uuid;
+
+  static const String CH_UUID_STR;
+  static uuids::uuid_name_generator UUID_NAME_GENERATOR;
+};
+
+} // namespace sfmx
+
+namespace std {
+template <>
+struct hash<sfmx::UUID> {
+  using argument_type = sfmx::UUID;
+  using result_type = sfmx::size_t;
+
+  NODISCARD result_type
+  operator()(const argument_type& uuid) const noexcept {
+    return uuid.getHash();
+  }
+
+};
+} // namespace std
