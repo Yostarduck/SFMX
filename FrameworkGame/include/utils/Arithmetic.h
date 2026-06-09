72a15d5ea46f078d6690bb2bd8382ca22585a9f7
USwampertor <us.github@gamedev.8shield.net>
2026-06-09 21:31:01 +0200

    Precise method for lerp

--- FrameworkGame/include/utils/Arithmetic.h
+++ FrameworkGame/include/utils/Arithmetic.h
@@ -8,24 +8,26 @@
 namespace sfmx::lerp
 {
 
+NODISCARD inline float
+number(float a, float b, float t) {
+  return (a * (1.0 - t)) + (b * t);
+}
+
 NODISCARD inline sf::Vector2f
-vector2(const sf::Vector2f& a, const sf::Vector2f& b, float t)
-{
-  return {a.x + (b.x - a.x) * t, a.y + (b.y - a.y) * t};
+vector2(const sf::Vector2f& a, const sf::Vector2f& b, float t) {
+  return {lerp::number(a.x, b.x, t), lerp::number(a.y, b.y, t)};
 }
 
 NODISCARD inline sf::Color
-color(const sf::Color& a, const sf::Color& b, float t)
-{
-  return sf::Color(
-    static_cast<uint8>(static_cast<float>(a.r) +
-      (static_cast<float>(b.r) - static_cast<float>(a.r)) * t),
-    static_cast<uint8>(static_cast<float>(a.g) +
-      (static_cast<float>(b.g) - static_cast<float>(a.g)) * t),
-    static_cast<uint8>(static_cast<float>(a.b) +
-      (static_cast<float>(b.b) - static_cast<float>(a.b)) * t),
-    static_cast<uint8>(static_cast<float>(a.a) +
-      (static_cast<float>(b.a) - static_cast<float>(a.a)) * t)
+color(const sf::Color& a, const sf::Color& b, float t) {
+  return sf::Color(static_cast<uint8>(lerp::number(static_cast<float>(a.r),
+                                                   static_cast<float>(b.r), t)),
+                   static_cast<uint8>(lerp::number(static_cast<float>(a.g),
+                                                   static_cast<float>(b.g), t)),
+                   static_cast<uint8>(lerp::number(static_cast<float>(a.b),
+                                                   static_cast<float>(b.b), t)),
+                   static_cast<uint8>(lerp::number(static_cast<float>(a.a),
+                                                   static_cast<float>(b.a), t))
   );
 }
 
